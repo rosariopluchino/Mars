1414ccd3b1c51bc996e647c54f33445e66a075b5
rosariopluchino <rosshp95@rosshp95.localdomain>
2022-02-01 12:48:48 +0100

    build

---
+++ Mars/build.sh
@@ -0,0 +1,5 @@
+docker build ./logstash --tag marsweather:logstash
+docker build ./spark --tag marsweather:spark
+docker build ./kafka --tag marsweather:kafka
+docker build ./kibana --tag marsweather:kibana
+docker build ./elasticsearch --tag marsweather:elasticsearch
